c748092074258cbfd503327a785ce9e4ea770528
vaibhavdevnani7 <vaibhavdevnani7@gmail.com>
2023-04-17 03:33:37 +0530

    gtfol

---
+++ firmware_esp8266/src/WiFiManager.h
@@ -0,0 +1,48 @@
+#ifndef WIFI_H
+#define WIFI_H
+
+#include <Arduino.h>
+#include <DNSServer.h>
+#include <memory>
+
+class WifiManager
+{
+
+private:
+    DNSServer *dnsServer;
+    String ssid;
+    String pass;
+    IPAddress ip;
+    IPAddress gw;
+    IPAddress sub;
+    IPAddress dns;
+    bool reconnect = false;
+    bool inCaptivePortal = false;
+    char const *captivePortalName;
+    unsigned long timeout = 60000;
+    
+    void startCaptivePortal(char const *apName);
+    void stopCaptivePortal();
+    void connectNewWifi(String newSSID, String newPass);    
+    void storeToEEPROM();
+    int8_t waitForConnectResult(unsigned long timeoutLength);
+    std::function<void()> _forgetwificallback;
+    std::function<void()> _newwificallback;    
+
+public : 
+    void begin(char const *apName, unsigned long newTimeout = 60000);
+    void loop();
+    void forget();
+    bool isCaptivePortal();
+    String SSID();
+    void setNewWifi(String newSSID, String newPass);
+    void setNewWifi(String newSSID, String newPass, String newIp, String newSub, String newGw, String newDns);
+    void forgetWiFiFunctionCallback( std::function<void()> func );
+    void newWiFiFunctionCallback( std::function<void()> func );
+};
+
+extern WifiManager WiFiManager;
+
+#endif
+
+
